70d446a48130337d87e00dd0412dbc9f7c9cd08a
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-06-09 19:15:45 -0400

    origin v1

--- api/app.py
+++ api/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, send_from_directory
 from flask_cors import CORS
 import os
 
@@ -14,6 +14,12 @@ def welcome(name):
 @app.route('/')
 def index():
     return {'dataSetResults':[1,2,3,4,5,6,7,8,9,10]}
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ =='__main__':
     app.run(threaded=True, host='0.0.0.0', port=os.environ['PORT'])
