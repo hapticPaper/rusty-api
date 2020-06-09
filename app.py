f7891de3d5df9395ea71560afe72f767856d21de
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-06-09 15:22:51 -0400

    init

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route('/welcome')
+def welcome():
+    return render_template('index.html', name='ian')
+
+
+@app.route('/')
+def index():
+    return {'dataSetResults':[1,2,3,4,5,6,7,8,9,10]}
+
+if __name__ =='__main__':
+    app.run()
