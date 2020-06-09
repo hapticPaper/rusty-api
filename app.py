c1938d0aacb421ddbda94c98756be2ab9b6b2eaf
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-06-09 18:56:45 -0400

    origin

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
 
 @app.route('/welcome')
@@ -15,4 +15,4 @@ def index():
     return {'dataSetResults':[1,2,3,4,5,6,7,8,9,10]}
 
 if __name__ =='__main__':
-    app.run()
+    app.run(threaded=True, host='0.0.0.0', port=5000)
