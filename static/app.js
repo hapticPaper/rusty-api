3751a8c2b113af774970703d3730adad05dbde1f
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-06-09 21:41:01 -0400

    origin v1.0

--- static/app.js
+++ static/app.js
@@ -1,10 +1,8 @@
-
-
 fetch('/endpoints').then(data=>data.json()).then(d=>{
     d.endpoints.forEach(n=>{
         o = document.createElement("option")
         o.text = n
         document.getElementById('dataSelect').add(o)
-        updateData()
     })
+    updateData()
 })
--- static/updater.js
+++ static/updater.js
@@ -1,8 +1,37 @@
 
 function updateData(){
     endpoint = $('#dataSelect').val()
+    htmlList = []
+    chartData = []
+    cnvs = document.createElement("canvas")
+    ctx = cnvs.getContext('2d')
     fetch(`/${endpoint}`).then(data=>data.json()).then(d=>{
-    document.getElementById('onlydiv').innerHTML=d.dataSetResults.map(n=>{return `<b>Number: ${n}</b>`}).join("<br>");
-    console.log(d)
+        console.log(d);
+        chartData=d.dataSetResults;
+        d.dataSetResults.forEach(n=>{
+            htmlList.push(`<b>Number:</b>${n}`);
+    });
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'bar',
+    
+        // The data for our dataset
+        data: {
+            labels: chartData,
+            datasets: [{
+                
+                label: 'My First dataset',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: chartData
+            }]
+        },
+    
+        // Configuration options go here
+        options: {}
+    });
+    document.getElementById('onlydiv').innerHTML=htmlList.join("<br>");
+    document.getElementById('secondDiv').innerHTML=""
+    document.getElementById('secondDiv').appendChild(cnvs);
 })
 }
