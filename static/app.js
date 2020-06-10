db09bce5af13023d4b2051ab18849311bd54c1b6
Ian Rubenstein <rubenstein.ian@gmail.com>
2020-06-09 20:52:43 -0400

    origin v1.0

--- static/app.js
+++ static/app.js
@@ -1,13 +1,13 @@
 
 function updateData(){
     endpoint = $('#dataSelect').val()
-    fetch(`http://localhost:6060/${endpoint}`).then(data=>data.json()).then(d=>{
+    fetch(`/${endpoint}`).then(data=>data.json()).then(d=>{
     document.getElementById('onlydiv').innerHTML=d.dataSetResults.map(n=>{return `<b>Number: ${n}</b>`}).join("<br>");
     console.log(d)
 })
 }
 
-fetch('http://localhost:6060/endpoints').then(data=>data.json()).then(d=>{
+fetch('/endpoints').then(data=>data.json()).then(d=>{
     d.endpoints.forEach(n=>{
         o = document.createElement("option")
         o.text = n
